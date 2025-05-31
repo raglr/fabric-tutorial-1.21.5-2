31023069bd4ad8f94a48d3b6b9749f25eefcd109
Raglr <raglr0330@gmail.com>
2025-05-31 16:04:44 +0300

    commit the wrong item group event

--- src/main/java/net/raglr/tutorialmod/item/ModItems.java
+++ src/main/java/net/raglr/tutorialmod/item/ModItems.java
@@ -1,6 +1,8 @@
 package net.raglr.tutorialmod.item;
 
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -8,11 +10,18 @@ import net.raglr.tutorialmod.TutorialMod;
 
 public class ModItems {
 
-    private static Item registerItem (String name, Item item){
-        return Registry.register(Registries.ITEM, new Identifier(TutorialMod.MOD_ID, name),item);
+    public static final Item RUBY = registerItem("ruby", new Item(new Item.Settings()));
+
+    private static Item registerItem(String name, Item item){
+        return Registry.register(Registries.ITEM, Identifier.of(TutorialMod.MOD_ID, name), item);
     }
 
     public static void registerModItem(){
         TutorialMod.LOGGER.info("Registering mod items for " + TutorialMod.MOD_ID);
     }
+
+    ItemGroupEvents.ModifyEntries(ItemGroups.INGREDIENTS).register(entries -> { entries.add(RUBY);
+
+
 }
+
